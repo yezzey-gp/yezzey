1b93303f01ab69828f9b66b81f0bc3f40c772539
reshke kirill <reshke@double.cloud>
2022-06-09 13:12:31 +0300

    fix string info

--- yezzey_setup.sh
+++ yezzey_setup.sh
@@ -1,17 +1,17 @@
 #/bin/bash
 
-pg_ctl -D /home/fstilus/pg_build/e -o '--port=5432' stop
-rm -fr /home/fstilus/pg_build/e
+DB_PATH=/home/reshke/pgdata
+pg_ctl -D $DB_PATH -o '--port=5432' stop
+rm -fr $DB_PATH
 
-rm -f /home/fstilus/pg_build/postgres/l0
+rm -f $DB_PATH/logfile
 
-initdb -D /home/fstilus/pg_build/e
-echo yezzey.S3_putter = \'wal-g st put %f %s --config /home/fstilus/config.yaml -f\' >> /home/fstilus/pg_build/e/postgresql.conf
-echo yezzey.S3_getter = \'wal-g st get %f %s --config /home/fstilus/config.yaml\' >> /home/fstilus/pg_build/e/postgresql.conf
-pg_ctl -D /home/fstilus/pg_build/e -l /home/fstilus/pg_build/postgres/l0 -o '--port=5432' start
-
+initdb -D $DB_PATH
+echo yezzey.S3_putter = \'cp -f %f /home/reshke/s3/%s \' >> $DB_PATH/postgresql.conf
+echo yezzey.S3_getter = \'cp /home/reshke/s3/%f %s\' >> $DB_PATH/postgresql.conf
+pg_ctl -D $DB_PATH -l $DB_PATH/logfile -o '--port=5432' start
 
 psql "host=localhost port=5432 dbname=postgres" -c 'CREATE EXTENSION yezzey';
 
-sed -i "s/#shared_preload_libraries = ''/shared_preload_libraries = 'yezzey.so'/g" /home/fstilus/pg_build/e/postgresql.conf
-pg_ctl -D /home/fstilus/pg_build/e -l /home/fstilus/pg_build/postgres/l0 -o '--port=5432' restart
+sed -i "s/#shared_preload_libraries = ''/shared_preload_libraries = 'yezzey.so'/g" $DB_PATH/postgresql.conf
+pg_ctl -D $DB_PATH -l $DB_PATH/logfile -o '--port=5432' restart
