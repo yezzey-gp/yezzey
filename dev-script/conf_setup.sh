198d74e1858e0a4f97527c6af7b51023b9abfa92
reshke kirill <reshke@double.cloud>
2022-08-19 11:47:04 +0000

    Add debian

--- dev-script/conf_setup.sh
+++ dev-script/conf_setup.sh
@@ -1,8 +1,12 @@
 #conf_setup.sh
 
+
+
+gpconfig -c shared_preload_libraries -v 'yezzey.so'
+
 cd ../../gpAux/gpdemo/datadirs
 for DIR in */; do
 	ls $DIR*/postgresql.conf
-	echo -e "shared_preload_libraries = 'yezzey.so'\nyezzey.S3_getter = 'wal-g st get %f %s --config $1'\nyezzey.S3_putter = 'wal-g st put %f %s --config $1 -f'\nyezzey.S3_prefix = '$DIR'" | tee -a `ls $DIR*/postgresql.conf` > /dev/null
+	echo -e "yezzey.S3_getter = 'wal-g st get %f %s --config $1'\nyezzey.S3_putter = 'wal-g st put %f %s --config $1 -f'\nyezzey.S3_prefix = '$DIR'" | tee -a `ls $DIR*/postgresql.conf` > /dev/null
 done
 
