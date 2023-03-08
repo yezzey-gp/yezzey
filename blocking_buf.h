69aead5cb33e6f51898a897ddcca9352b4a5c7d4
reshke kirill <reshke@double.cloud>
2023-03-08 22:01:55 +0000

    Fix encrypt operation, use std::string instead of const char *

--- blocking_buf.h
+++ blocking_buf.h
@@ -13,7 +13,9 @@ class BlockingBuffer {
     size_t offset_;
 
     std::mutex mu_;
-    std::condition_variable cv_;
+    std::condition_variable cv_read_;
+    std::condition_variable cv_write_;
+    // std::condition_variable cv_full_;
 
     const size_t default_sz_ = 1 << 12;
 
@@ -33,7 +35,7 @@ public:
     }
     // BlockingBuffer& operator=(const BlockingBuffer&) = delete;
 
-    explicit BlockingBuffer(size_t sz) : sz_(sz), offset_(0), closed_(false) {
+    BlockingBuffer(size_t sz) : sz_(sz), offset_(0), closed_(false) {
         buffer_ = (char*)malloc(sizeof(char) * sz_);
     }
 
@@ -44,25 +46,27 @@ public:
     int read(char * dest, size_t sz) {
         std::unique_lock<std::mutex> lk(mu_);
 
-        cv_.wait(lk, [&] { return sz_ - offset_ || closed_; } );
+        cv_read_.wait(lk, [&] { return offset_ || closed_; } );
 
-        if (closed_) {
-            return 0;
+        if (offset_) {
+            size_t mpy_sz = std::min(sz, offset_);
+            std::memcpy(dest, buffer_, mpy_sz);
+            std::memmove(buffer_, buffer_ + mpy_sz, offset_ - mpy_sz);
+            offset_ -= mpy_sz;
+
+            // notify writer
+            cv_write_.notify_one();
+            return mpy_sz;
         }
 
-        size_t mpy_sz = std::min(sz, offset_);
-        std::memcpy(dest, buffer_, mpy_sz);
-        std::memmove(buffer_, buffer_ + offset_, mpy_sz);
-        offset_ -= mpy_sz;
-        // notify writer
-        cv_.notify_one();
-        return mpy_sz;
+        // else closed_
+        return 0;
     }
 
     int write(const char * source, size_t sz) {
         std::unique_lock<std::mutex> lk(mu_);
 
-        cv_.wait(lk, [&] { return sz_ - offset_ || closed_; } );
+        cv_write_.wait(lk, [&] { return sz_ - offset_ || closed_; } );
 
         if (closed_) {
             return 0;
@@ -71,21 +75,30 @@ public:
         size_t mpy_sz = std::min(sz, sz_ - offset_);
         std::memcpy(buffer_ + offset_, source, mpy_sz);
         offset_ += mpy_sz;
+
+        cv_read_.notify_one();
         return mpy_sz;
     }
+
+    // void waitdone() {
+    //     std::unique_lock<std::mutex> lk(mu_);
+    //     cv_full_.wait(lk, [&] { return offset_ == 0; } );
+    // }
 
     void clear() {
         std::unique_lock<std::mutex> lk(mu_);
 
         offset_ = 0;
-        cv_.notify_one();
+        cv_read_.notify_all();
+        cv_write_.notify_all();
     }
 
     void close () {
         std::unique_lock<std::mutex> lk(mu_);
 
         closed_ = true;
-        cv_.notify_all();
+        cv_read_.notify_all();
+        cv_write_.notify_all();
     }
 
     ~BlockingBuffer() {
