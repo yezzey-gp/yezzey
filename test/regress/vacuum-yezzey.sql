89101cfbb63e33d1e25e6bd1bf18c4ef7aca3f9f
reshke kirill <reshke@double.cloud>
2023-03-22 12:48:26 +0000

    Add regress tests

---
+++ test/regress/vacuum-yezzey.sql
@@ -0,0 +1,19 @@
+DROP DATABASE testreg;
+CREATE DATABASE testreg;
+\c testreg
+
+CREATE EXTENSION yezzey;
+CREATE TABLE aot(i INT) WITH (appendonly=true);
+select yezzey_define_offload_policy('aot');
+insert into aot select * from generate_series(1, 10000);
+insert into aot select * from generate_series(1, 10000);
+delete from aot;
+insert into aot select * from generate_series(10000, 20000);
+VACUUM aot;
+select count(1) from aot; -- works ok, use second block(segment) of relation ;
+insert into aot select * from generate_series(10000, 20000); -- insert goes in first block(segment) with modcount 4
+select count(1) from aot; -- fails
+
+
+\c postgres
+DROP DATABASE testreg;
---
+++ test/regress/yezzey-reorg.sql
@@ -0,0 +1,73 @@
+DROP DATABASE testreg;
+CREATE DATABASE testreg;
+\c testreg
+
+CREATE EXTENSION yezzey;
+
+-- AO
+
+CREATE TABLE regaoty(i INT) WITH (appendonly=true);
+INSERT INTO regaoty SELECT * FROM generate_series(1, 100000);
+SELECT * FROM yezzey_define_offload_policy('regaoty');
+
+SELECT * FROM yezzey_offload_relation_status('regaoty');
+SELECT * FROM yezzey_offload_relation_status_per_filesegment('regaoty');
+SELECT * FROM yezzey_relation_describe_external_storage_structure('regaoty');
+
+SELECT count(1) FROM regaoty;
+INSERT INTO regaoty SELECT * FROM generate_series(1, 100000);
+SELECT count(1) FROM regaoty;
+
+SELECT * FROM yezzey_offload_relation_status('regaoty');
+SELECT * FROM yezzey_offload_relation_status_per_filesegment('regaoty');
+SELECT * FROM yezzey_relation_describe_external_storage_structure('regaoty');
+
+DELETE FROM regaoty;
+INSERT INTO regaoty SELECT * FROM generate_series(1, 100000);
+INSERT INTO regaoty SELECT * FROM generate_series(1, 100000);
+SELECT count(1) FROM regaoty;
+
+ALTER TABLE regaoty SET WITH (REORGANIZE=true);
+SELECT count(1) FROM regaoty;
+
+SELECT * FROM yezzey_offload_relation_status('regaoty');
+SELECT * FROM yezzey_offload_relation_status_per_filesegment('regaoty');
+SELECT * FROM yezzey_relation_describe_external_storage_structure('regaoty');
+
+\! echo AO simple test OK
+
+-- AOCS
+
+CREATE TABLE regaocsty(i INT) WITH (appendonly=true, orientation=column);
+INSERT INTO regaocsty SELECT * FROM generate_series(1, 100000);
+SELECT * FROM yezzey_define_offload_policy('regaocsty');
+
+SELECT * FROM yezzey_offload_relation_status('regaocsty');
+SELECT * FROM yezzey_offload_relation_status_per_filesegment('regaocsty');
+SELECT * FROM yezzey_relation_describe_external_storage_structure('regaocsty');
+
+SELECT count(1) FROM regaocsty;
+INSERT INTO regaocsty SELECT * FROM generate_series(1, 100000);
+SELECT count(1) FROM regaocsty;
+
+SELECT * FROM yezzey_offload_relation_status('regaocsty');
+SELECT * FROM yezzey_offload_relation_status_per_filesegment('regaocsty');
+SELECT * FROM yezzey_relation_describe_external_storage_structure('regaocsty');
+
+DELETE FROM regaocsty;
+INSERT INTO regaocsty SELECT * FROM generate_series(1, 100000);
+INSERT INTO regaocsty SELECT * FROM generate_series(1, 100000);
+SELECT count(1) FROM regaocsty;
+
+ALTER TABLE regaocsty SET WITH (REORGANIZE=true);
+SELECT count(1) FROM regaocsty;
+
+SELECT * FROM yezzey_offload_relation_status('regaocsty');
+SELECT * FROM yezzey_offload_relation_status_per_filesegment('regaocsty');
+SELECT * FROM yezzey_relation_describe_external_storage_structure('regaocsty');
+
+
+\! echo AOCS simple test OK
+\c postgres
+
+DROP DATABASE testreg;
