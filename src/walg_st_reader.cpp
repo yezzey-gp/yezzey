524d8109c9dde9a03e2427c6ae5b42ad79a547b7
reshke <reshke@double.cloud>
2023-10-25 17:36:07 +0000

    Fix read function

--- src/walg_st_reader.cpp
+++ src/walg_st_reader.cpp
@@ -37,37 +37,50 @@ bool WALGSTReader::close() {
   return true;
 }
 bool WALGSTReader::read(char *buffer, size_t *amount) {
-  if (wal_g_ == nullptr) {
-    if (order_ptr_ == order_.size()) {
-      return false;
-    }
-    cmd_ = craftString(order_[order_ptr_].x_path, GpIdentity.segindex);
-    if (wal_g_ != nullptr) {
-      pclose(wal_g_);
-      wal_g_ = nullptr;
-    }
-    wal_g_ = popen(cmd_.c_str(), "r");
+  while (1) {
     if (wal_g_ == nullptr) {
-      elog(yezzey_log_level, "failed to open wal-g reader for %s", order_[order_ptr_].x_path.c_str());
-      return false;
+      if (order_ptr_ == order_.size()) {
+        return false;
+      }
+      cmd_ = craftString(order_[order_ptr_].x_path, GpIdentity.segindex);
+      if (wal_g_ != nullptr) {
+        pclose(wal_g_);
+        wal_g_ = nullptr;
+      }
+      wal_g_ = popen(cmd_.c_str(), "r");
+      if (wal_g_ == nullptr) {
+        elog(WARNING, "failed to read wal-g for %s, %d, %m", order_[order_ptr_].x_path.c_str(), ferror(wal_g_)); 
+        return false;
+      }
+
+      ++order_ptr_;
     }
 
-    ++order_ptr_;
-  }
+    auto rc = fread(buffer, sizeof(char), *amount, wal_g_);
+    if (rc < 0) {
+        elog(WARNING, "failed to read wal-g for %s, %d, %m", order_[order_ptr_].x_path.c_str(), ferror(wal_g_)); 
+        return rc;
+    }
 
-  auto rc = fread(buffer, sizeof(char), *amount, wal_g_);
-  if (rc <= 0) {
-      elog(yezzey_log_level, "failed to read wal-g for %s, %m", order_[order_ptr_].x_path.c_str());
-  }
+    if (rc == 0) {
+      if (feof(wal_g_)) {
+        wal_g_ = nullptr;
+        continue;
+      }
+      elog(WARNING, "failed to read wal-g for %s, %d, %m", order_[order_ptr_].x_path.c_str(), ferror(wal_g_)); 
+      return -1;
+    }
 
-  *amount = rc;
+    *amount = rc;
 
-  if (feof(wal_g_)) {
-    wal_g_ = nullptr;
-    return true;
+    if (feof(wal_g_)) {
+      wal_g_ = nullptr;
+      return true;
+    }
+    return rc > 0;
   }
-  return rc > 0;
 }
+
 int WALGSTReader::prepare() { return 0; }
 
 bool WALGSTReader::empty() {
