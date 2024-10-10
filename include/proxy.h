b148fa48b119f1966e655e0456a21fdcd63fa813
reshke kirill <reshke@double.cloud>
2024-10-10 14:26:06 +0000

    Adapt yezzey for CDBD compatibility

--- include/proxy.h
+++ include/proxy.h
@@ -6,27 +6,30 @@
 #include "gucs.h"
 #include "storage.h"
 
+#include "ygpver.h"
+
 #ifdef __cplusplus
 #define EXTERNC extern "C"
 #else
 #define EXTERNC
 #endif
 
+#if IsModernYezzey
+typedef File SMGRFile;
+#endif
+
 EXTERNC int64 yezzey_NonVirtualCurSeek(SMGRFile file);
 EXTERNC void yezzey_FileClose(SMGRFile file);
 EXTERNC int64 yezzey_FileSeek(SMGRFile file, int64 offset, int whence);
 
-#if GP_VERSION_NUM >= 70000
+#if IsModernYezzey
 EXTERNC int yezzey_FileSync(SMGRFile file, uint32 wait_event_info);
 #else
 EXTERNC int yezzey_FileSync(SMGRFile file);
 #endif
 
-#if GP_VERSION_NUM >= 70000
-EXTERNC SMGRFile yezzey_AORelOpenSegFile(Oid reloid, const char *nspname,
-                                         const char *relname,
-                                         const char *fileName, int fileFlags,
-                                         int64 modcount);
+#if IsModernYezzey
+EXTERNC SMGRFile yezzey_AORelOpenSegFile(const char *fileName, int fileFlags);
 #else
 EXTERNC SMGRFile yezzey_AORelOpenSegFile(Oid reloid, char *nspname,
                                          char *relname, const char *fileName,
@@ -34,7 +37,7 @@ EXTERNC SMGRFile yezzey_AORelOpenSegFile(Oid reloid, char *nspname,
                                          int64 modcount);
 #endif
 
-#if GP_VERSION_NUM >= 70000
+#if IsModernYezzey
 EXTERNC int yezzey_FileWrite(SMGRFile file, char *buffer, int amount,
                              off_t offset, uint32 wait_event_info);
 EXTERNC int yezzey_FileRead(SMGRFile file, char *buffer, int amount,
@@ -44,14 +47,14 @@ EXTERNC int yezzey_FileWrite(SMGRFile file, char *buffer, int amount);
 EXTERNC int yezzey_FileRead(SMGRFile file, char *buffer, int amount);
 #endif
 
-#if GP_VERSION_NUM >= 70000
+#if IsModernYezzey
 EXTERNC int yezzey_FileTruncate(SMGRFile file, int64 offset,
                                 uint32 wait_event_info);
 #else
 EXTERNC int yezzey_FileTruncate(SMGRFile file, int64 offset);
 #endif
 
-#if GP_VERSION_NUM >= 70000
+#if IsModernYezzey
 EXTERNC int yezzey_FileDiskSize(SMGRFile file);
 #endif
 
